a9d4f9da666f77bc5dc12c39eda0266b668ba38e
周勇 <pry5652@163.com>
2018-05-30 00:35:04 +0800

    .

---
+++ WebProxy/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using WebProxy.Models;
+
+namespace WebProxy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Download([FromForm]string Url, [FromForm]string Ext)
+        {
+            var client = new HttpClient();
+            var stream = client.GetStreamAsync(Url).Result;
+            string fileExt = Ext;
+
+            //获取文件的ContentType
+            var provider = new FileExtensionContentTypeProvider();
+            var memi = provider.Mappings[fileExt];
+
+            return File(stream, memi, Path.GetFileName(Url));
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
